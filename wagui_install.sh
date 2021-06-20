51bf1c69a7467e02940f619551f07a76d38c548f
tommy <500@superscafe.com>
2021-06-20 09:33:04 -0400

    update – comments

--- wagui_install.sh
+++ wagui_install.sh
@@ -12,10 +12,10 @@ export CONTAINER_DIR="${HOME_DIR}/Docker/Wagui"
 
 echo ${CONTAINER_DIR}
 
-## Create dir for Docker container
+## create dir for Docker container
 mkdir -p ${CONTAINER_DIR} && cd "${CONTAINER_DIR}"
 
-# Add ENV for docker-compose.yml use
+# add ENV for docker-compose.yml use
 echo "CONTAINER_DIR=${CONTAINER_DIR}" >> .env
 
 echo "NETWORK=${NETWORK}" >> .env
@@ -28,32 +28,34 @@ echo "DOMAIN_NAME_3=${DOMAIN_NAME_3}" >> .env
 
 echo "DEFAULT_EMAIL=${DEFAULT_EMAIL}" >> .env
 
-## Add website dir
+## add website dir
 mkdir html && cd html
-# Add ENV for docker-compose.yml use
-## Import web files
+## import web files
 curl -LO https://github.com/NH3R717/Wagui-Restaurant/archive/refs/heads/master.zip
 ## uncompress webfiles and remove master.zip 
 unzip master.zip && rm -rf master.zip
 ## remove unnecessary files
-cd Wagui-Restaurant-master && sudo rm README.md .gitignore dockerfile wagui_install.sh docker-compose.yml LICENSE  
-## Copy files from Wagui-Restaurant-master to WebFiles
+cd Wagui-Restaurant-master && sudo rm -f README.md .gitignore dockerfile wagui_install.sh docker-compose.yml LICENSE  
+## copy files from Wagui-Restaurant-master to WebFiles
 cp -a . .. && cd .. && sudo rm -rf Wagui-Restaurant-master && cd ..
 pwd
-# Import docker-compose.yml
+# import docker-compose.yml
 sudo curl -L https://raw.githubusercontent.com/NH3R717/Wagui-Restaurant/master/docker-compose.yml > docker-compose.yml
-# Create nginx network
+# create nginx network
 #! sudo docker network create ${NETWORK}
-# Build and run container w/ ENV
+# build and run container w/ ENV
 sudo docker-compose up -d --build
  
-#  set to user permissions
+# set to user permissions
 sudo chmod 0750 "${CONTAINER_DIR}"
 sudo chown --recursive \
 "${USERNAME}":"${USERNAME}" "${CONTAINER_DIR}"
-# Remove .env = (problems w/ docker-compose down)
+# remove .env = (problems w/ docker-compose down)
 # rm -f .env
 
 ###################################*
 ### Useful Commands & Notes here ###
 ###################################*
+
+# sudo curl -L https://raw.githubusercontent.com/NH3R717/Wagui-Restaurant/master/wagui_install.sh | exec bash
+# sudo docker stop web_wagui && sudo docker container prune && sudo docker image prune && sudo rm -rf ~/Docker/Wagui
