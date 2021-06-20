5c41b3058b0b743ba12615334ca150fd871b223a
tommy <500@superscafe.com>
2021-06-19 20:44:32 -0400

    update – "sudo docker stop web_test..."

--- wagui_install.sh
+++ wagui_install.sh
@@ -57,4 +57,4 @@ sudo chown --recursive \
 ###################################*
 
 # sudo curl -L https://raw.githubusercontent.com/NH3R717/Wagui-Restaurant/master/wagui_install.sh | exec bash
-# sudo docker stop web_test && sudo docker container prune && sudo docker image prune && sudo rm -rf ~/Docker/Wagui
+# sudo docker stop web_wagui && sudo docker container prune && sudo docker image prune && sudo rm -rf ~/Docker/Wagui
