03bc32f6195a4e408995f0bf811f5174f5f978a5
tommy <500@superscafe.com>
2021-06-23 12:33:48 -0400

    refactor – to web script

--- web_wagui_install.sh
+++ web_wagui_install.sh
@@ -1,55 +1,45 @@
 #!/bin/bash
-# exec &>/tmp/wagui_boot_script.log
+#exec &>/tmp/wagui_boot_script.log
 # Run Fail Safe Command
-# set -euxo pipefail
+#set -euxo pipefail
+
+echo \
+'
 
 #########################
 ### Web Install & Run ###
 #########################
 
-CONTAINER_DIR="${HOME_DIR}/Docker/Wagui"
+'
 
-# change dir to new container dir
+export CONTAINER_DIR="${HOME_DIR}/Docker/Test"
+echo **
+echo "Created container dir ${CONTAINER_DIR}"
+echo **
+
+################################################
+##! Project Dependent ENV / Change as Needed ###
+
+export PROJECT_NAME=web_wagui
+export GIT_BRANCH=dev
+
+##! Project Dependent ENV / Change as Needed ###
+################################################
+
+# create dir for Docker container & change dir
 mkdir -p ${CONTAINER_DIR} && cd "${CONTAINER_DIR}"
 
-# add ENV for docker-compose.yml use
-export CONTAINER_DIR
-echo "CONTAINER_DIR=${CONTAINER_DIR}" >> .env
-export NETWORK
-echo "NETWORK=${NETWORK}" >> .env
-export DOMAIN_NAME_1
-echo "DOMAIN_NAME_1=${DOMAIN_NAME_1}" >> .env
-export DOMAIN_NAME_2
-echo "DOMAIN_NAME_2=${DOMAIN_NAME_2}" >> .env
-export DOMAIN_NAME_3
-echo "DOMAIN_NAME_3=${DOMAIN_NAME_3}" >> .env
-export DEFAULT_EMAIL
-echo "DEFAULT_EMAIL=${DEFAULT_EMAIL}" >> .env
+echo \
+'
 
+####################################
+### Install Script Pt.1 Complete ###
+####################################
 
-## Add website dir
-mkdir html && cd html && sudo rm -f ${PWD}
-## Import web files
-curl -LO https://github.com/NH3R717/Wagui-Restaurant/archive/refs/heads/dev.zip
-## uncompress webfiles and remove *.zip 
-unzip dev.zip && rm -rf dev.zip
-## remove unnecessary files
-cd Wagui-Restaurant-dev && sudo rm README.md .gitignore dockerfile wagui_install.sh docker-compose.yml LICENSE  
-## Copy files from *-master to WebFiles
-cp -a . .. && cd .. && sudo rm -rf Wagui-Restaurant-dev && cd ..
-pwd
-# import docker-compose.yml
-sudo curl -L https://raw.githubusercontent.com/NH3R717/Wagui-Restaurant/dev/docker-compose.yml > docker-compose.yml
+'
 
-# build and run container w/ ENV
-sudo docker-compose up -d --build
- 
-# set to user permissions
-sudo chmod 0750 "${CONTAINER_DIR}"
-sudo chown --recursive \
-"${USERNAME}":"${USERNAME}" "${CONTAINER_DIR}"
-# remove .env = (problems w/ docker-compose down)
-# rm -f .env
+# run web_up script w/ ENV
+sudo curl -L https://raw.githubusercontent.com/NH3R717/VPS_Scripts/master/web/web_up.sh | exec bash
 
 ###################################*
 ### Useful Commands & Notes here ###
